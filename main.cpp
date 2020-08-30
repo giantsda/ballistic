bcf00c220741b327ace52644ce1e43731397e4f9
giantsda <chenshen@rams.colostate.edu>
2020-08-30 11:09:13 -0600

    first upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ballistics.h"
+
+using namespace std;
+int main (){
+	double angle=ZeroAngle(G1, 0.200, 1070, 1.8017, 25, 0.);
+
+	// printf("the shooting angle is %f\n",angle);
+
+	printf ("# Range elevationMil Time WindageMil Velocity \n");
+
+	double** solution=(double**)malloc(20);
+	// double ZeroAngle(int DragFunction, double DragCoefficient, double Vi, double SightHeight, double ZeroRange, double yIntercept){
+
+
+// 	int SolveAll(int DragFunction, double DragCoefficient, double Vi, double SightHeight, \
+// double ShootingAngle, double ZAngle, double WindSpeed, double WindAngle, double** Solution){
+	SolveAll(G1, 0.2, 1070, 1.8017,0, angle, 10, 90,  solution);
+	double* data=*solution;
+
+	for (int i=1;i<300;i=i+8)
+	{
+		printf("%f ",data[i*10+0]);
+		printf("%f ",data[i*10+2]/3.438);
+		printf("%f ",data[i*10+3]);
+		printf("%f ",data[i*10+5]/3.438);
+		printf("%f ",data[i*10+6]);
+		printf("\n");
+	}
+	return 0;
+}
+
